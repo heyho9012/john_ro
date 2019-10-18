ba1b9b8e2c19e4baff7541d7864f0f9902974db1
LSH <lsohi2468@gmail.com>
2019-10-18 18:03:43 +0900

    js_1018_18t

--- d_js_basic/js/src/script/gnb_02.js
+++ d_js_basic/js/src/script/gnb_02.js
@@ -2,13 +2,14 @@
 
   const gnb=$('#gnb'),
         gnbArea=gnb.children('ul'),
+        menuIcon=gnb.children('#menuIcon'),
         gnbArLi=gnbArea.children('li');
   gnbArLi.children('a').addClass('titleLink');
 
   const titleLink=$('.titleLink'),
         partList=titleLink.next('ul'),
         subLink=partList.find('a');
-
+  
   gnbArLi.on('mouseenter focus',function(){
     partList.stop().slideDown();
   });
@@ -16,5 +17,11 @@
   gnbArLi.on('mouseleave',function(){
     partList.stop().slideUp();
   });
+
+  menuIcon.on('click',function(){
+
+  })
+
+
 
 })(jQuery);
